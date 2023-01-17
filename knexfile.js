83529de77151a4c76ba979cee9378f75190a6a64
Ihor Pryyma <ih.pryyma@gmail.com>
2023-01-17 21:48:53 +0200

    feat: add bootstrap to list all items

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,11 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/my_life'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 };
+
--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const knex = require('../db/knex');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('todo')
+    .select()
+    .then(todos => {
+      res.render('all', { todos: todos });
+    });
 });
 
 module.exports = router;
