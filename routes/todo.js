a499ac729c11a36d5922ec32bfa8bfdab7909083
Ihor Pryyma <ih.pryyma@gmail.com>
2023-01-17 22:21:09 +0200

    feat: add POST new item api

--- routes/todo.js
+++ routes/todo.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 const knex = require('../db/knex');
 /* GET home page. */
+
+
+function validTodo(todo) {
+  return typeof todo.title === 'string' && 
+                todo.title.trim() !== '' && 
+                typeof todo.priority !== 'undefined' && 
+                !isNaN(Number(todo.priority));
+}
 router.get('/', (req, res) => {
   knex('todo')
     .select()
@@ -13,6 +21,28 @@ router.get('/', (req, res) => {
 
 router.get('/new', (req, res) => {
   res.render('new');
+});
+
+router.post('/', (req, res) => {
+  if(validTodo(req.body)) {
+    const todo = {
+      title: req.body.title,
+      description: req.body.description,
+      priority: req.body.priority,
+      date: new Date()
+    };
+    knex('todo')
+      .insert(todo, 'id')
+      .then(ids => {
+        const id = ids[0];
+        res.redirect(`/todo/${id}`);
+      });
+  } else {
+    res.status(500);
+    res.render('error', {
+      message: 'Invalid Todo'
+    });
+  }
 });
 
 module.exports = router;
